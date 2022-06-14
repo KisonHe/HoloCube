fcf489331f3043efd6f009d258971ab86ee2ee38
KisonHe <kisonmaxine@gmail.com>
2022-06-14 20:54:39 +0800

    Try to get DMA running

--- src/gui/gui.cpp
+++ src/gui/gui.cpp
@@ -5,7 +5,9 @@
 #include <FreeRTOS.h>
 #include <freertos/timers.h>
 
-#include <TFT_eSPI.h>
+// #include <TFT_eSPI.h>
+#define LGFX_USE_V1
+#include <LovyanGFX.hpp>
 
 #include <lvgl.h>
 #include "lvgl_fs.h"
@@ -15,14 +17,68 @@
 
 #include "app/helloworld_app/helloworld_app.h"
 
-static const uint16_t screenWidth  = TFT_WIDTH;
-static const uint16_t screenHeight = TFT_HEIGHT;
+static const uint16_t screenWidth  = 240;
+static const uint16_t screenHeight = 240;
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * 10 ];
 TaskHandle_t lvgl_Task_Handle;
 SemaphoreHandle_t lvgl_lock;
 
-TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
+class LGFX : public lgfx::LGFX_Device
+{
+lgfx::Panel_ST7789     _panel_instance;
+lgfx::Bus_SPI       _bus_instance;   // SPIバスのインスタンス
+// lgfx::Light_PWM     _light_instance;
+// lgfx::Touch_XPT2046     _touch_instance;
+
+public:
+  LGFX(void)
+  {
+    { // バス制御の設定を行います。
+      auto cfg = _bus_instance.config();    // バス設定用の構造体を取得します。
+      cfg.spi_host = VSPI_HOST;     // 使用するSPIを選択  (VSPI_HOST or HSPI_HOST)
+      cfg.spi_mode = 0;             // SPI通信モードを設定 (0 ~ 3)
+      cfg.freq_write = 40000000;    // 送信時のSPIクロック (最大80MHz, 80MHzを整数で割った値に丸められます)
+      cfg.freq_read  = 16000000;    // 受信時のSPIクロック
+      cfg.spi_3wire  = false;        // 受信をMOSIピンで行う場合はtrueを設定
+      cfg.use_lock   = true;        // トランザクションロックを使用する場合はtrueを設定
+      cfg.dma_channel = 0;          // Set the DMA channel (1 or 2. 0=disable)   使用するDMAチャンネルを設定 (0=DMA不使用)
+      cfg.pin_sclk = 18;            // SPIのSCLKピン番号を設定
+      cfg.pin_mosi = 5;            // SPIのMOSIピン番号を設定
+      cfg.pin_miso = -1;            // SPIのMISOピン番号を設定 (-1 = disable)
+      cfg.pin_dc   = 23;            // SPIのD/Cピン番号を設定  (-1 = disable)
+      _bus_instance.config(cfg);    // 設定値をバスに反映します。
+      _panel_instance.setBus(&_bus_instance);      // バスをパネルにセットします。
+    }
+
+    { // 表示パネル制御の設定を行います。
+      auto cfg = _panel_instance.config();    // 表示パネル設定用の構造体を取得します。
+      cfg.pin_cs           =    -1;  // CSが接続されているピン番号   (-1 = disable)
+      cfg.pin_rst          =    4;  // RSTが接続されているピン番号  (-1 = disable)
+      cfg.pin_busy         =    -1;  // BUSYが接続されているピン番号 (-1 = disable)
+      cfg.memory_width     =   240;  // ドライバICがサポートしている最大の幅
+      cfg.memory_height    =   240;  // ドライバICがサポートしている最大の高さ
+      cfg.panel_width      =   240;  // 実際に表示可能な幅
+      cfg.panel_height     =   240;  // 実際に表示可能な高さ
+      cfg.offset_x         =     0;  // パネルのX方向オフセット量
+      cfg.offset_y         =     0;  // パネルのY方向オフセット量
+      cfg.offset_rotation  =     0;  // 回転方向の値のオフセット 0~7 (4~7は上下反転)
+      cfg.dummy_read_pixel =     8;  // ピクセル読出し前のダミーリードのビット数
+      cfg.dummy_read_bits  =     1;  // ピクセル以外のデータ読出し前のダミーリードのビット数
+      cfg.readable         =  true;  // データ読出しが可能な場合 trueに設定
+      cfg.invert           = false;  // パネルの明暗が反転してしまう場合 trueに設定
+      cfg.rgb_order        = false;  // パネルの赤と青が入れ替わってしまう場合 trueに設定
+      cfg.dlen_16bit       = false;  // データ長を16bit単位で送信するパネルの場合 trueに設定
+      cfg.bus_shared       =  true;  // SDカードとバスを共有している場合 trueに設定(drawJpgFile等でバス制御を行います)
+
+      _panel_instance.config(cfg);
+    }
+    setPanel(&_panel_instance); // 使用するパネルをセットします。
+  }
+};
+
+LGFX tft;
+
 static const char* TAG = "GUI";
 #if LV_USE_LOG != 0
 #if LV_LOG_PRINTF == 0
@@ -45,7 +101,9 @@ static void my_disp_flush( lv_disp_drv_t *disp, const lv_area_t *area, lv_color_
 
     tft.startWrite();
     tft.setAddrWindow( area->x1, area->y1, w, h );
-    tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+    // tft.pushColors( ( uint16_t * )&color_p->full, w * h, true );
+    tft.pushPixels( ( uint16_t * )&color_p->full, w * h, true );
+    // tft.writePixels((lgfx::rgb565_t *)&color_p->full, w * h);
     tft.endWrite();
     // tft.pushPixelsDMA(,);
 
--- todo.md
+++ todo.md
@@ -1,3 +1,5 @@
 Fork `tft_espi` and modify `TFT_Drivers/ST7789_Rotation.h` as dependency, or change main stream to stop git watching .pio files
 
 git ignore src/gui/fonts
+
+https://youtu.be/IPCvQ4o_WP8 Plz Try to get dma running
